6c47642116beeac88195bfa60bc4cfa47dd0e0f1
sembruk <ya-kimov@mail.ru>
2018-02-20 10:51:24 +0300

    Test

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 
 from sportiduino import Sportiduino
+import os, pty
+import serial
 
+
+#master, slave = pty.openpty()
+#s_name = os.ttyname(slave)
+#print(s_name)
+#sportiduino = Sportiduino(port=s_name, debug=True)
 
 sportiduino = Sportiduino(debug=True)
 
